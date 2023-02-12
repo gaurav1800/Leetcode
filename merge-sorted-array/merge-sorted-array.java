f7b3563b5e2670377d16caaa88da096851177b38
Gaurav <gauravsanghu@yahoo.com>
2023-02-12 01:46:51 -0500

    Time: 0 ms, Memory: 42.7 MB - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int[] res = new int[m+n];
+        
+        int i = 0, j = 0, k = 0;
+        
+        if (nums2.length != 0) {
+            while (k != m+n) {
+                if (j == n) {
+                    res[k] = nums1[i];
+                    i++;
+                }
+                else if (nums1[i] <= nums2[j] && i != m) {
+                    res[k] = nums1[i];
+                    i++;
+                } 
+                else {
+                    res[k] = nums2[j];
+                    j++;
+                }
+            k++;
+        }
+        
+            for(int z = 0; z < m+n; z++) {
+                nums1[z] = res[z];
+            }    
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+}
