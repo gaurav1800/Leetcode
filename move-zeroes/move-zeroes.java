83212e06d64ab7dc86953c355fedfe89022fc08a
Gaurav <gauravsanghu@yahoo.com>
2023-02-15 16:32:55 -0500

    Time: 1 ms, Memory: 43.4 MB - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        // if (nums.length <= 1) {
+        //     return nums;
+        // }
+        
+        int j = 0; 
+        
+        for(int i=0; i<nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        
+        while(j != nums.length) {
+            nums[j] = 0;
+            j++;
+        }
+        
+        // return nums;
+        
+    }
+}
