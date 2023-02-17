bbf75169eee0d783570d2a10b0cea571e2fb0038
Gaurav <gauravsanghu@yahoo.com>
2023-02-16 19:16:22 -0500

    Time: 0 ms, Memory: 40.8 MB - LeetHub

--- remove-element/remove-element.java
+++ remove-element/remove-element.java
@@ -12,18 +12,17 @@ class Solution {
             return 0;
         }
         
-        int j = 0, k = 0;
+        int j = 0;
         
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != val) {
                 nums[j] = nums[i];
                 j++;
-                k++;
             }
         }
         
         
-        return k;
+        return j;
         
     }
 }
