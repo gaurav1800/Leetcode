e57e6476554b45fce717fecfba5aa8106d32d517
Gaurav <gauravsanghu@yahoo.com>
2023-02-20 17:58:31 -0500

    Time: 5 ms, Memory: 50.4 MB - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -2,25 +2,20 @@ class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         
         
-        
-        List<Integer> result = new ArrayList<>(nums.length);
+        List<Integer> result = new ArrayList<>();
         
         int j;
         
         for(int i=0; i<nums.length; i++) {
-            
             j = Math.abs(nums[i]) - 1;
             
-            // j = nums[i] - 1;
-            
             if(nums[j] > 0) {
-                nums[j] *= -1;    
+                nums[j] *= -1;
             }
-            
         }
         
         for(int i=0; i<nums.length; i++) {
-            if(nums[i] > 0) {
+            if (nums[i] > 0) {
                 result.add(i+1);
             }
         }
@@ -32,20 +27,34 @@ class Solution {
         
         
         
-//         List<Integer> result = new ArrayList<Integer>();
         
-//         List<Integer> input = new ArrayList<Integer>();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         List<Integer> result = new ArrayList<>(nums.length);
+        
+//         int j;
         
 //         for(int i=0; i<nums.length; i++) {
-//             input.add(i, nums[i]);
+            
+//             j = Math.abs(nums[i]) - 1;
+            
+            
+//             if(nums[j] > 0) {
+//                 nums[j] *= -1;    
+//             }
+            
 //         }
         
-//         for(int i=1; i<=nums.length; i++) {
-//             if(input.contains(i)) {
-//                 continue;
-//             }
-//             else {
-//                 result.add(i);
+//         for(int i=0; i<nums.length; i++) {
+//             if(nums[i] > 0) {
+//                 result.add(i+1);
 //             }
 //         }
         
