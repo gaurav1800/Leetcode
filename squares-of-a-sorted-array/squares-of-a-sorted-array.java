c065dabad248ea4cfee26713d3afa4de0bc3f334
Gaurav <gauravsanghu@yahoo.com>
2023-02-21 13:48:33 -0500

    Time: 2 ms, Memory: 55.5 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,26 +1,68 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        
+   
         int[] result = new int[nums.length];
         
-        int i=0, j=nums.length-1, iresult=nums.length-1;
+        int i=0, j=nums.length-1, resultidx = nums.length-1;
         
-        
-        while(iresult >= 0) {
+        while(resultidx > -1) {
             int isquared = nums[i] * nums[i];
             int jsquared = nums[j] * nums[j];
             
-            if(isquared > jsquared) {
-                result[iresult] = isquared; 
+            if (isquared > jsquared) {
+                result[resultidx] = isquared;
                 i++;
+                resultidx--;
             }
             else {
-                result[iresult] = jsquared;
+                result[resultidx] = jsquared;
                 j--;
+                resultidx--;
             }
-            iresult--;
         }
         
         return result;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int[] result = new int[nums.length];
+        
+//         int i=0, j=nums.length-1, iresult=nums.length-1;
+        
+        
+//         while(iresult >= 0) {
+//             int isquared = nums[i] * nums[i];
+//             int jsquared = nums[j] * nums[j];
+            
+//             if(isquared > jsquared) {
+//                 result[iresult] = isquared; 
+//                 i++;
+//             }
+//             else {
+//                 result[iresult] = jsquared;
+//                 j--;
+//             }
+//             iresult--;
+//         }
+        
+//         return result;
     }
 }
