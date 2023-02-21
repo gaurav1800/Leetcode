c2391a40722d0aa510af31b5035c7cd93db5bc75
Gaurav <gauravsanghu@yahoo.com>
2023-02-21 14:16:49 -0500

    Time: 1 ms (96.83%), Space: 46.1 MB (5.78%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -26,18 +26,5 @@ class Solution {
         }
         
         
-        
-        
-        
-        // for(int i=0; i<arr.length; i++) {
-        //     if(arr[i] == 0) {
-        //         for(int j = arr.length-1; j>i; j--) {
-        //             arr[j] = arr[j-1];
-        //         }
-        //         i++;
-        //     }
-        // }
-        
-        
     }
 }
