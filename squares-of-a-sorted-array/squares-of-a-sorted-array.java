bb8493448ffead7993e79dbb7888134aa9015d13
Gaurav <gauravsanghu@yahoo.com>
2023-02-21 13:49:03 -0500

    Time: 2 ms, Memory: 55.4 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -12,57 +12,15 @@ class Solution {
             if (isquared > jsquared) {
                 result[resultidx] = isquared;
                 i++;
-                resultidx--;
             }
             else {
                 result[resultidx] = jsquared;
                 j--;
-                resultidx--;
             }
+            resultidx--;
         }
         
         return result;
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         int[] result = new int[nums.length];
-        
-//         int i=0, j=nums.length-1, iresult=nums.length-1;
-        
-        
-//         while(iresult >= 0) {
-//             int isquared = nums[i] * nums[i];
-//             int jsquared = nums[j] * nums[j];
-            
-//             if(isquared > jsquared) {
-//                 result[iresult] = isquared; 
-//                 i++;
-//             }
-//             else {
-//                 result[iresult] = jsquared;
-//                 j--;
-//             }
-//             iresult--;
-//         }
-        
-//         return result;
     }
 }
