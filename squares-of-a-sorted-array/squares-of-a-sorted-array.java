4cd99d760e13cb8db5ae3b55461f179ff4222eea
Gaurav <gauravsanghu@yahoo.com>
2023-02-21 14:05:25 -0500

    Time: 2 ms, Memory: 55.9 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,25 +1,60 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-   
+        
+        if (nums[0] >= 0) {
+            int i=0; 
+            while(i < nums.length) {
+                nums[i] = square(nums[i]);
+                i++;
+            }
+            return nums;
+        }
+        
+        
+        int i=0, j=nums.length-1, idx = nums.length-1;
+        
         int[] result = new int[nums.length];
         
-        int i=0, j=nums.length-1, resultidx = nums.length-1;
-        
-        while(resultidx > -1) {
-            int isquared = nums[i] * nums[i];
-            int jsquared = nums[j] * nums[j];
+        while(idx > -1) {
             
-            if (isquared > jsquared) {
-                result[resultidx--] = isquared;
+            
+            if (square(nums[i]) > square(nums[j])) {
+                result[idx] = square(nums[i]);
                 i++;
             }
             else {
-                result[resultidx--] = jsquared;
+                result[idx] = square(nums[j]);
                 j--;
             }
+            idx--;
         }
         
         return result;
+                
         
+//         int[] result = new int[nums.length];
+        
+//         int i=0, j=nums.length-1, resultidx = nums.length-1;
+        
+//         while(resultidx > -1) {
+//             int isquared = nums[i] * nums[i];
+//             int jsquared = nums[j] * nums[j];
+            
+//             if (isquared > jsquared) {
+//                 result[resultidx--] = isquared;
+//                 i++;
+//             }
+//             else {
+//                 result[resultidx--] = jsquared;
+//                 j--;
+//             }
+//         }
+        
+//         return result;
+        
+    }
+    
+    public int square(int m) {
+        return m*m;
     }
 }
