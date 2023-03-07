74a14d447b214d26121fd86f017733ed387abe0c
Gaurav <gauravsanghu@yahoo.com>
2023-03-07 15:08:47 -0500

    Time: 2 ms (95.76%), Space: 42.6 MB (32.10%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -12,26 +12,68 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
         ListNode head = new ListNode(0);
-        ListNode headptr = head;
-        
+        ListNode current = head;
         int extra = 0;
         
         while (l1 != null || l2 != null || extra != 0) {
-            int i = (l1 != null) ? l1.val : 0;
-            int j = (l2 != null) ? l2.val : 0;
-            int sum = extra + i + j;
-            extra = sum / 10;
-            headptr.next = new ListNode(sum % 10);
-            headptr = headptr.next;
-            if (l1 != null)
+            int x = l1!=null ? l1.val : 0;
+            int y = l2!=null ? l2.val : 0;
+            int sum = x+y+extra;
+            
+            extra = sum/10;
+            current.next = new ListNode(sum%10);
+            
+            current = current.next;
+            
+            if (l1 != null){
                 l1 = l1.next;
-            if (l2 != null)
+            }
+            if (l2 != null) {
                 l2 = l2.next;
+            }
         }
-        
         return head.next;
         
         
+        
+        
+//         ListNode p1 = l1, p2 = l2;
+        
+//         String snum1= "", snum2 = "";
+        
+//         while (p1 != null) {
+//             snum1 = p1.val + snum1;
+//             p1 = p1.next;
+//         }
+        
+//         while (p2 != null) {
+//             snum2 = p2.val + snum2;
+//             p2 = p2.next;
+//         }
+        
+//         // long sum = Long.valueOf(snum1) + Long.valueOf(snum2);
+        
+//         long sum = Long.valueOf(snum1) + Long.valueOf(snum2);
+        
+        
+//         ListNode result = new ListNode(0, null);
+//         ListNode resp = result;
+        
+//         while (sum != 0) {
+//             resp.val = Integer.valueOf((int)sum % 10);
+            
+            
+//             if (sum/10 == 0) {
+//                 break;
+//             }
+//             ListNode node = new ListNode(0, null);
+//             resp.next = node;
+//             resp = resp.next;
+            
+//             sum /= 10;
+//         }
+        
+//         return result;
         
     }
 }
