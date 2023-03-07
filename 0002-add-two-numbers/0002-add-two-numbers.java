c53ceb363bb0149e8a59f82f40c8575f461e8dcb
Gaurav <gauravsanghu@yahoo.com>
2023-03-07 15:21:21 -0500

    Time: 2 ms (95.76%), Space: 42.8 MB (24.95%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -13,26 +13,72 @@ class Solution {
         
         ListNode head = new ListNode(0);
         ListNode current = head;
-        int extra = 0;
+        int sum = 0, extra = 0;
         
         while (l1 != null || l2 != null || extra != 0) {
-            int x = l1!=null ? l1.val : 0;
-            int y = l2!=null ? l2.val : 0;
-            int sum = x+y+extra;
+            
+            int num1 = l1 != null ? l1.val : 0;
+            int num2 = l2 != null ? l2.val : 0;
+            
+            sum = num1+num2+extra;
             
             extra = sum/10;
-            current.next = new ListNode(sum%10);
             
+            ListNode node = new ListNode (sum%10);
+            current.next = node;
             current = current.next;
             
-            if (l1 != null){
+            if (l1 != null) {
                 l1 = l1.next;
             }
             if (l2 != null) {
                 l2 = l2.next;
             }
         }
+        
         return head.next;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         ListNode head = new ListNode(0);
+//         ListNode current = head;
+//         int extra = 0;
+        
+//         while (l1 != null || l2 != null || extra != 0) {
+//             int x = l1!=null ? l1.val : 0;
+//             int y = l2!=null ? l2.val : 0;
+//             int sum = x+y+extra;
+            
+//             extra = sum/10;
+//             current.next = new ListNode(sum%10);
+            
+//             current = current.next;
+            
+//             if (l1 != null){
+//                 l1 = l1.next;
+//             }
+//             if (l2 != null) {
+//                 l2 = l2.next;
+//             }
+//         }
+//         return head.next;
         
     }
 }
