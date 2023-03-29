647579c60b4bde99129cb1b7059d885652e283e1
Gaurav <gauravsanghu@yahoo.com>
2023-03-29 17:01:52 -0400

    Time: 1 ms (100.00%), Space: 43.6 MB (13.35%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,5 +1,6 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        
         if (nums.length == 0) {
             return -1;
         }
@@ -10,15 +11,17 @@ class Solution {
         
         int j = nums.length-1;
         
-        if (nums[j] - nums[0] == 0) {
+        if (nums[j] == nums[0]) {
             return 0;
         }
         
         for(int i=1; i<nums.length; i++) {
             if (nums[j] - nums[i] == nums[i-1]) {
                 return i;
-            } 
+            }
         }
+        
         return -1;
+        
     }
 }
