654b7adde1aee945c48c6376da2194559516315e
Gaurav <gauravsanghu@yahoo.com>
2023-04-03 23:38:35 -0400

    Time: 31 ms (18.13%), Space: 39.6 MB (23.85%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,28 +3,49 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int lo=0, hi=n;
+//         int lo=0, hi=n;
+        
+//         while (lo<hi) {
+//             int mid = (lo+hi)/2;
+//             boolean isBad = isBadVersion(mid);
+//             if (isBad && !isBadVersion(mid-1)) {
+//                 return mid;
+//             }
+//             // else if (!isBad) {
+//             //     lo = mid+1;
+//             // }
+//             // else{
+//             //     hi = mid-1;
+//             // }
+//             else if (!isBad) {
+//                 lo = mid+1;
+//             }
+//             else{
+//                 hi = mid-1;
+//             }
+//         }
+//         return lo;
+        
+        long lo=1, hi=n;
         
         while (lo<hi) {
-            int mid = (hi-lo)/2+lo;
-            boolean isBad = isBadVersion(mid);
-            // if (isBad && !isBadVersion(mid-1)) {
-            //     return mid;
-            // }
-            // else if (!isBad) {
-            //     lo = mid+1;
-            // }
-            // else{
-            //     hi = mid-1;
-            // }
-            if (!isBad) {
+            long mid = (lo+hi)/2;
+            if (isBadVersion((int)mid) == true && isBadVersion((int)mid-1) == false) {
+                return (int) mid;
+            }
+            else if (isBadVersion((int)mid) == false) {
                 lo = mid+1;
             }
             else{
-                hi = mid;
+                hi = mid-1;
             }
         }
-        return lo;
+        if (isBadVersion((int)lo) == true) {
+            return (int) lo;
+        }
+        else {
+            return (int)hi;
+        }
         
     }
 }
