aaae2fc77c73b69741784ae7896195edc0e3239e
Gaurav <gauravsanghu@yahoo.com>
2023-04-11 15:26:35 -0400

    Time: 0 ms (100.00%), Space: 41.8 MB (87.25%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -3,11 +3,27 @@ class Solution {
         
         int n = cost.length;
         
+        
         for(int i=2; i<cost.length; i++) {
             cost[i] += Math.min(cost[i-1], cost[i-2]);
         }
         
         return Math.min(cost[n-1], cost[n-2]);
                 
+        
+        
+        // Greedy approach (does not work cuz not optimal)
+//         while(i > 1) {
+//             if (cost[i-1] < cost[i-2]) {
+//                 sum += cost[i-1];
+//                 i--;
+//             }
+//             else {
+//                 sum += cost[i-2];
+//                 i -= 2;
+//             }
+//         }
+        
+//         return sum;
     }
 }
