b02366b688cf27f7e4ff30d4e89a976964343424
Gaurav <gauravsanghu@yahoo.com>
2023-04-13 22:51:20 -0400

    Time: 1 ms (100.00%), Space: 43.2 MB (65.68%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,26 +1,23 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+    
         
-        if (nums.length == 0) {
-            return -1;
-        }
-        
-        for(int i=1; i<nums.length; i++) {
+        for(int i = 1; i < nums.length; i++) {
             nums[i] += nums[i-1];
         }
-        
+    
         int j = nums.length-1;
-        
+    
         if (nums[j] == nums[0]) {
             return 0;
         }
+        
         
         for(int i=1; i<nums.length; i++) {
             if (nums[j] - nums[i] == nums[i-1]) {
                 return i;
             }
         }
-        
         return -1;
         
     }
