39708fa3df8c9979e5a887ec514aadce2956d1c7
Gaurav <gauravsanghu@yahoo.com>
2023-04-15 18:38:48 -0400

    Time: 6 ms (86.39%), Space: 42.6 MB (46.58%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,23 +1,49 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         
-        HashMap<Character, Character> map = new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
         
         for(int i=0; i<s.length(); i++) {
-            
-            if (!map.containsKey(s.charAt(i)) && !map.containsValue(t.charAt(i))) {
-                map.put(s.charAt(i), t.charAt(i));
-            }
-            else if ((!map.containsKey(s.charAt(i)) && map.containsValue(t.charAt(i))) || (map.containsKey(s.charAt(i)) && !map.containsValue(t.charAt(i)))) {
+            if (map1[s.charAt(i)] != map2[t.charAt(i)]) {
                 return false;
             }
-            else {
-                if (map.get(s.charAt(i)) != t.charAt(i)) {
-                    return false;
-                }
-            }
+            map1[s.charAt(i)] = i+1;
+            map2[t.charAt(i)] = i+1;
         }
+        
         return true;
+        
+        
+        
+        
+        
+        // too slow
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+        
+//         HashMap<Character, Character> map = new HashMap<>();
+        
+//         for(int i=0; i<s.length(); i++) {
+            
+//             if (!map.containsKey(s.charAt(i)) && !map.containsValue(t.charAt(i))) {
+//                 map.put(s.charAt(i), t.charAt(i));
+//             }
+//             else if ((!map.containsKey(s.charAt(i)) && map.containsValue(t.charAt(i))) || (map.containsKey(s.charAt(i)) && !map.containsValue(t.charAt(i)))) {
+//                 return false;
+//             }
+//             else {
+//                 if (map.get(s.charAt(i)) != t.charAt(i)) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
         
         
         
