741792c6eeff78c587a37ccfca68bae448589623
Gaurav <gauravsanghu@yahoo.com>
2023-04-17 20:24:16 -0400

    Time: 2 ms (79.97%), Space: 40.4 MB (96.80%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,13 +1,13 @@
 class Solution {
     public int longestPalindrome(String s) {
         
-        char[] arr = new char[256];
+        char[] arr = new char[128];
         int ans = 0;
         
         for(int i=0; i<s.length(); i++) {
             arr[s.charAt(i)]++;
         }
-        for(int i=0; i<256; i++) {
+        for(int i=0; i<128; i++) {
             ans += (arr[i]/2); 
         }
         ans *= 2;
