040d932b6de62c422b81ff46aeb339332ccfca42
Gaurav <gauravsanghu@yahoo.com>
2023-04-27 18:22:04 -0400

    Time: 0 ms (100.00%), Space: 43.6 MB (40.97%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -5,32 +5,6 @@ class Solution {
         
         fill(image,sr,sc,image[sr][sc], color);
         return image;
-        
-//         // slower
-//         int oldColor = image[sr][sc];
-        
-//         if (oldColor == color) {
-//             return image;
-//         }
-        
-//         int[][] grid = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-//         image[sr][sc] = color;
-        
-//         for(int i=0; i<grid.length; i++) {
-//             int nxtX = sr + grid[i][0];
-//             int nxtY = sc + grid[i][1];
-//             if (nxtX < 0 || nxtX >= image.length || nxtY < 0 || nxtY >= image[0].length) {
-//                 continue;
-//             }
-//             if (image[nxtX][nxtY] != oldColor) {
-//                 continue;
-//             }
-//             floodFill(image, nxtX, nxtY, color);
-//         }
-        
-//         return image;
-        
-        
     }
     
     public void fill(int[][] image, int i, int j, int oldColor, int newColor) {
