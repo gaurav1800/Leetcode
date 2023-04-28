0a30ece0906e5f6166e7775645398a0a3bcdd1be
Gaurav <gauravsanghu@yahoo.com>
2023-04-28 13:06:41 -0400

    Time: 10 ms (79.08%), Space: 44 MB (16.03%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,30 +1,72 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        int slen = s.length(), plen = p.length();
         
-        List<Integer> list = new ArrayList<>();
+        if (slen < plen) return new ArrayList();
         
-        for(int i=0; i<s.length()-p.length()+1; i++) {
-            if (isAnagram(p, s.substring(i, i+p.length()))) {
-                list.add(i);
+        int[] scounter = new int[26];
+        int[] pcounter = new int[26];
+        
+        for(char c : p.toCharArray()) {
+            pcounter[(int)(c - 'a')]++;
+        }
+        
+        List<Integer> result = new ArrayList();
+        
+        for(int i=0; i<slen; i++) {
+            scounter[(int) (s.charAt(i) - 'a')]++;
+            
+            if (i >= plen) {
+                scounter[(int) (s.charAt(i-plen) - 'a')]--;
+            }
+            
+            if (Arrays.equals(pcounter, scounter)) {
+                result.add(i-plen+1);
             }
         }
-        return list;
         
+        return result;
     }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         // Another Solution
+//         List<Integer> list = new ArrayList<>();
+        
+//         for(int i=0; i<s.length()-p.length()+1; i++) {
+//             if (isAnagram(p, s.substring(i, i+p.length()))) {
+//                 list.add(i);
+//             }
+//         }
+//         return list;
+        
+//     }
     
-    public boolean isAnagram(String p, String s) {
-        int[] arr = new int[128];
+//     public boolean isAnagram(String p, String s) {
+//         int[] arr = new int[128];
         
-        for(int i=0; i<p.length(); i++) {
-            arr[p.charAt(i)]++;
-        }
+//         for(int i=0; i<p.length(); i++) {
+//             arr[p.charAt(i)]++;
+//         }
         
-        for(int i=0; i<s.length(); i++) {
-            arr[s.charAt(i)]--;
-            if (arr[s.charAt(i)] < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+//         for(int i=0; i<s.length(); i++) {
+//             arr[s.charAt(i)]--;
+//             if (arr[s.charAt(i)] < 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
 }
