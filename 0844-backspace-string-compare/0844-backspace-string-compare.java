3d2251629cb18dc93457e8780dc1a9d92e1c9aff
Gaurav <gauravsanghu@yahoo.com>
2023-04-28 13:56:21 -0400

    Time: 1 ms (84.06%), Space: 41 MB (37.25%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        
+        return backspace(s).equals(backspace(t));
+        
+    }
+    
+    private String backspace(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : s.toCharArray()) {
+            if (c != '#') {
+                sb.append(c);
+            }
+            else if (sb.length() != 0) {
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+        return sb.toString();
+    }
+}
