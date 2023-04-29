c518018980caf662e1352c86356ec5e8cdfb737e
Gaurav <gauravsanghu@yahoo.com>
2023-04-29 15:14:53 -0400

    Update 0083-remove-duplicates-from-sorted-list.java

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -11,24 +11,22 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         
-        ListNode ptr = new ListNode();
-        ptr = head;
-        
         if (head == null || head.next == null) {
             return head;
         }
         
-        while(ptr.next != null ) {
-            while(ptr.next != null && ptr.val == ptr.next.val){
+        ListNode ptr = head;
+        
+        while (ptr != null && ptr.next != null) {
+            if (ptr.val == ptr.next.val) {
                 ptr.next = ptr.next.next;
             }
-            if (ptr.next == null) {
-                break;
+            else {
+                ptr = ptr.next;
             }
-            ptr = ptr.next;       
         }
-        
         return head;
+        
         
     }
 }
