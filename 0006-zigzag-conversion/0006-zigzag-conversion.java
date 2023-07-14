dc78835e8a5e2676a9bd08750e62978131ccddae
Gaurav <gauravsanghu@yahoo.com>
2023-07-14 16:46:01 -0400

    Time: 3 ms (97.29%), Space: 44.1 MB (63.17%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,32 +1,28 @@
 class Solution {
     public String convert(String s, int numRows) {
-        
-        StringBuilder sb = new StringBuilder();
-        
-        List<Character>[] rows = new List[numRows];
-        
-        int k=0;
-        
-        int direction = numRows == 1 ? 0 : -1;
-        
-        for(int i=0; i<numRows; i++) {
-            rows[i] = new ArrayList<>(); 
+        if(numRows == 1){
+            return s;
         }
-        
-        for(char c : s.toCharArray()) {
-            rows[k].add(c);
-            if (k == 0 || k == numRows - 1) {
-                direction *= -1;
-            }
-            k += direction;
-        }
-        
-        for(List<Character> row : rows) {
-            for(char c : row) {
-                sb.append(c);
+
+        StringBuilder ans = new StringBuilder();
+        int len = s.length();
+        int sec1 = 2*(numRows -1);
+
+        for(int i=0; i<numRows; i++){
+            int index =i;
+            while(index <len){
+                ans.append(s.charAt(index));
+                if(i != 0 && i != numRows-1){
+                    int sec2 = sec1 - (2 *i);
+                    int sec3 = index + sec2;
+
+                    if(sec3 < len){
+                        ans.append(s.charAt(sec3));
+                    }
+                }
+                index = index + sec1;
             }
         }
-        
-        return sb.toString();
+        return ans.toString();
     }
 }
