078071c797434ffbe663919bfcacda89e0332d08
Gaurav <gauravsanghu@yahoo.com>
2024-02-02 23:34:37 -0500

    Time: 7 ms (58.48%), Space: 42.1 MB (47.96%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,22 +1,3 @@
-// class Solution {
-//     public int maxSumAfterPartitioning(int[] arr, int k) {
-        
-//         int n = arr.length;
-//         int[] result = new int[n+1];
-        
-//         for(int i = 1; i <= n; i++) {
-//             int max = Integer.MIN_VALUE;
-            
-//             for(int j = 1; j <= Math.max(i, k); j++) {
-//                 max = Math.max(max, arr[i-j]);
-//                 result[i] = Math.max(result[i], result[i-j] + max * j);
-//             }
-//         }
-        
-//         return result[n];
-//     }
-// }
-
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         
