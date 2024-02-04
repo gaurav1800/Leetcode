73939a2fb4df083d79d8285f8724f77c19470bda
Gaurav <gauravsanghu@yahoo.com>
2024-02-04 06:00:43 -0500

    Time: 96 ms (73.48%), Space: 17.3 MB (63.60%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        counter = collections.Counter(t)
+        required = len(t)
+        bestLeft = -1
+        minLength = len(s) + 1
+        
+        l = 0
+        
+        for r,c in enumerate(s):
+            counter[c] -= 1
+            if counter[c] >= 0:
+                required -= 1
+            while required == 0:
+                if r - l + 1 < minLength:
+                    bestLeft = l
+                    minLength = r - l + 1
+                counter[s[l]] += 1
+                
+                if counter[s[l]] > 0:
+                    required += 1
+                l += 1
+        
+        return '' if bestLeft == -1 else s[bestLeft: bestLeft + minLength]
+        
