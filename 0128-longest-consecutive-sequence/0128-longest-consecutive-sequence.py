4d3f914474d458310f2d9d3798f21e6ad321403a
Gaurav <gauravsanghu@yahoo.com>
2024-02-19 16:43:14 -0500

    Time: 340 ms (85.18%), Space: 31.8 MB (86.47%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,29 +1,45 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         
-        if len(nums) < 2:
-            return len(nums)
+        # faster implementation
+        numsSet = set(nums)
         
-        
-        sortedNums = sorted(nums)
-        maxLen = 0
         maxCurrent = 0
+        maxLen = 0
         
-        for idx, num in enumerate(sortedNums):
-            if idx < len(sortedNums)-1:
-                if sortedNums[idx] == sortedNums[idx+1]:
-                    continue
+        for n in numsSet:
+            if n-1 not in numsSet:
+                maxCurrent = 1
+                nCurrent = n
                 
-                if sortedNums[idx] + 1 == sortedNums[idx+1]:
+                while nCurrent+1 in numsSet:
                     maxCurrent += 1
-                    maxLen = maxLen if maxLen > maxCurrent else maxCurrent
-                else:
-                    maxCurrent = 0
+                    nCurrent += 1
+                    
+                maxLen = max(maxCurrent, maxLen)
         
-        return maxLen+1
-#         myDict = {}
+        return maxLen
         
-#         for num in nums:
-#             if num-1 in myDict:
-#                 myDict[num-1] = myDict
         
+        
+        
+        # slower implementation
+#         if len(nums) < 2:
+#             return len(nums)
+                
+#         sortedNums = sorted(nums)
+#         maxLen = 0
+#         maxCurrent = 0
+        
+#         for idx, num in enumerate(sortedNums):
+#             if idx < len(sortedNums)-1:
+#                 if sortedNums[idx] == sortedNums[idx+1]:
+#                     continue
+                
+#                 if sortedNums[idx] + 1 == sortedNums[idx+1]:
+#                     maxCurrent += 1
+#                     maxLen = maxLen if maxLen > maxCurrent else maxCurrent
+#                 else:
+#                     maxCurrent = 0
+        
+#         return maxLen+1
