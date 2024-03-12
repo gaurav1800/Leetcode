76c047240bcf26a52b2c3028a730b1e845d17ee1
Gaurav <gauravsanghu@yahoo.com>
2024-03-12 00:27:35 -0500

    Time: 36 ms (58.82%), Space: 16.6 MB (71.88%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -4,7 +4,7 @@ class Solution:
         result = 0
         
         for i in range(32):
-            bit  =  (n >> i ) & 1
+            bit = (n >> i) & 1
             result = result | (bit << (31-i))
         
         return result
