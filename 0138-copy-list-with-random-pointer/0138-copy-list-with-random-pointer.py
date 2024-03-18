16d875f554bb82c4bcadfa64f377cbdc50512d22
Gaurav <gauravsanghu@yahoo.com>
2024-03-18 00:03:33 -0500

    Time: 36 ms (72.58%), Space: 17.4 MB (34.94%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,7 +11,7 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         
         myDict = {None:None}
-        
+
         current = head
         
         while (current):
