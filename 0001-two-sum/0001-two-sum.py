cb60f70015dc19f0a993d91734cee605360093a6
Gaurav <gauravsanghu@yahoo.com>
2024-04-06 00:43:09 -0500

    Time: 54 ms (85.76%), Space: 17.7 MB (55.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        for idx,num in enumerate(nums):
-            if target-num in nums:
-                if num + nums[nums.index(target-num)] == target and idx != nums.index(target-num):
-                    return [idx, nums.index(target-num)]
+#         slow implementation
+        # for idx,num in enumerate(nums):
+        #     if target-num in nums:
+        #         if num + nums[nums.index(target-num)] == target and idx != nums.index(target-num):
+        #             return [idx, nums.index(target-num)]
+
+        # faster implementation
+        prevMap = {} 
+        for idx, val in enumerate(nums):
+            diff = target - val
+            if diff in prevMap:
+                return [prevMap[diff], idx]
+            else:
+                prevMap[val] = idx
