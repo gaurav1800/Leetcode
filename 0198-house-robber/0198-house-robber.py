b16a7f04be5233b5f75f0c9c2b3c7af662297244
Gaurav <gauravsanghu@yahoo.com>
2024-04-09 18:21:13 -0400

    Time: 41 ms (21.05%), Space: 16.5 MB (44.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -14,21 +14,3 @@ class Solution:
         
         
         
-#         if (len(nums) == 1):
-#             return nums[0]
-        
-#         if (len(nums) == 2):
-#             return max(nums[0], nums[1])
-            
-        
-#         mylist = []
-        
-#         mylist.append(nums[0])
-#         mylist.append(nums[1])
-        
-        
-#         for i in range(2, len(nums)):
-#             mylist.append(max(nums[i] + mylist[i-2], max(mylist)))
-            
-#         return max(mylist[-1], mylist[-2])
-        
