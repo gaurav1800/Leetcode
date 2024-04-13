3b956e26afcf00f54cbd05df8023e8244df2403f
Gaurav <gauravsanghu@yahoo.com>
2024-04-13 00:23:56 -0400

    Time: 33 ms (67.68%), Space: 16.6 MB (30.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,33 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
+
         if n <= 3:
             return n
+
+        result = [1, 2, 3]
+        
+        for i in range(3, n):
+            result.append(result[i-2] + result[i-1])
+        
+        return result[len(result) - 1]
+        
+        
+        
+        
+        
+#         another solution
+
+#         if n <= 3:
+#             return n
     
-        first = 2
-        second = 3
+#         first = 2
+#         second = 3
         
-        for i in range(4, n+1):
-            temp = first + second
-            first = second
-            second = temp
+#         for i in range(4, n+1):
+#             temp = first + second
+#             first = second
+#             second = temp
         
-        return second
+#         return second
         
