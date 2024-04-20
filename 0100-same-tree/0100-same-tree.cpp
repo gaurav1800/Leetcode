7943faab383d04747a2330c15a35b8d696d9c3dd
Gaurav <gauravsanghu@yahoo.com>
2024-04-20 00:21:27 -0400

    Time: 0 ms (100.00%), Space: 11.9 MB (14.95%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -17,15 +17,13 @@ public:
             return true;
         }
         
-        if (p == NULL || q == NULL) {
+        if (p != NULL && q != NULL && p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+        else {
             return false;
         }
         
-        if (p->val != q->val) {
-            return false;
-        }
-        
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         
     }
 };
