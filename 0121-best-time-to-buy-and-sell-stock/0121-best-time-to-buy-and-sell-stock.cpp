fcfcef09e690ee52871fed47e146ea934ded1231
Gaurav <gauravsanghu@yahoo.com>
2024-05-12 01:16:02 -0400

    Time: 91 ms (36.62%), Space: 95.7 MB (93.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -6,11 +6,29 @@ public:
         int current_min = prices[0];
         
         for(int i = 1; i < prices.size(); i++) {
-            max_profit = std::max(max_profit, prices[i] - current_min);
-            current_min = std::min(current_min, prices[i]);
+            
+            if (prices[i] - current_min > max_profit) {
+                max_profit = prices[i] - current_min;
+            }
+            
+            if (prices[i] < current_min) {
+                current_min = prices[i];
+            }
         }
         
         return max_profit;
         
+        
+        
+//         slower solution
+//         int max_profit = 0;
+//         int current_min = prices[0];
+        
+//         for(int i = 1; i < prices.size(); i++) {
+//             max_profit = std::max(max_profit, prices[i] - current_min);
+//             current_min = std::min(current_min, prices[i]);
+//         }
+        
+//         return max_profit;
     }
 };
