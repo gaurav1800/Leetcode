b5b14d13712ccde9ed6b81fadd1210f4427ecc60
Gaurav <gauravsanghu@yahoo.com>
2024-08-15 01:34:34 -0400

    Time: 9 ms (51.74%), Space: 9.8 MB (23.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,12 +6,29 @@ public:
             return false;
         }
         
-        string sortedS = s;
-        string sortedT = t;
+//         // little bit slower
+//         string sortedS = s;
+//         string sortedT = t;
         
-        sort(sortedS.begin(), sortedS.end());
-        sort(sortedT.begin(), sortedT.end());
+//         sort(sortedS.begin(), sortedS.end());
+//         sort(sortedT.begin(), sortedT.end());
         
-        return sortedS == sortedT;
+//         return sortedS == sortedT;
+        
+        unordered_map<char, int> map;
+        
+        for (char c: s) {
+            map[c]++;
+        }
+        
+        for (char c: t) {
+            if (map.find(c) == map.end() || map[c] == 0) {
+                return false;
+            }
+            else {
+                map[c]--;
+            }
+        }
+        return true;
     }
 };
