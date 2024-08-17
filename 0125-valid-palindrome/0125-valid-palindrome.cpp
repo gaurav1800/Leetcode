211eb44b8070b120a09e1a3a40a9887dfbd4cdc7
Gaurav <gauravsanghu@yahoo.com>
2024-08-17 00:40:56 -0400

    Time: 4 ms (79.63%), Space: 8.9 MB (68.50%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,20 +5,19 @@ public:
         int i = 0;
         int j = s.size() - 1;
         
-        while (i < j) {
-            while (!isalnum(s[i]) && i < j) {
+        while(i<j) {
+            while (!isalnum(s[i]) && i<j) {
                 i++;
             }
-            while (!isalnum(s[j]) && i < j) {
+            while(!isalnum(s[j]) && i<j) {
                 j--;
             }
-            if (tolower(s[i]) != tolower(s[j])) {
+            if(tolower(s[i]) != tolower(s[j])) {
                 return false;
             }
             i++;
             j--;
         }
-        
         return true;
         
     }
