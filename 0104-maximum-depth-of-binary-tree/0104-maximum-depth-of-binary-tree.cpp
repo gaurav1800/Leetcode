35a24d38092d55c8af6018179ffeb94a86b38388
Gaurav <gauravsanghu@yahoo.com>
2024-08-18 02:04:34 -0400

    Time: 4 ms (76.17%), Space: 17.8 MB (29.21%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -17,7 +17,7 @@ public:
             return 0;
         }
         
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        return 1 + max(maxDepth(root->left),maxDepth(root->right));
         
     }
 };
