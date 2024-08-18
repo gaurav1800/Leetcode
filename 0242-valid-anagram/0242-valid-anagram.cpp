7a6a0b10dad5ce2e02127cfe768e07c5e23623e1
Gaurav <gauravsanghu@yahoo.com>
2024-08-18 17:46:59 -0400

    Time: 3 ms (97.21%), Space: 8.7 MB (68.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,22 +6,39 @@ public:
             return false;
         }
         
+        vector<int> arr(26, 0);
         
-        unordered_map<char, int> map;
-        
-        for(char c:s) {
-            map[c]++;
+        for(int i=0; i<s.size(); i++) {
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
         }
         
-        for(char c:t){
-            if(map.find(c) == map.end() || map[c] == 0) {
+        for(int i = 0; i < arr.size(); i++) {
+            if (arr[i] != 0) {
                 return false;
-            }    
-            else {
-                map[c]--;
             }
         }
+        
         return true;
+        
+//         // takes up more space due to the map
+//         unordered_map<char, int> map;
+        
+//         for(char c:s) {
+//             map[c]++;
+//         }
+        
+//         for(char c:t){
+//             if(map.find(c) == map.end() || map[c] == 0) {
+//                 return false;
+//             }    
+//             else {
+//                 map[c]--;
+//             }
+//         }
+//         return true;
+        
+        
         
         
     }
