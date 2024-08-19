b067493974ea48321c04d33e5e502dc7cca8a47d
Gaurav <gauravsanghu@yahoo.com>
2024-08-19 18:08:38 -0400

    Time: 0 ms (100.00%), Space: 7.4 MB (49.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,21 +6,40 @@ public:
             return n;
         }
         
-        int* arr= new int[n+1];
-        
-        arr[1] = 1;
-        arr[2] = 2;
-        arr[3] = 3;
+        int first = 2;
+        int second = 3;
+        int result = 0;
         
         for(int i = 4; i <= n; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
+            result = first + second;
+            first = second;
+            second = result;
         }
         
-        int result = arr[n];
-        
-        delete[] arr;
-        
         return result;
+        
+        
+        
+        
+        
+        
+        
+        // using DP approach
+//         int* arr= new int[n+1];
+        
+//         arr[1] = 1;
+//         arr[2] = 2;
+//         arr[3] = 3;
+        
+//         for(int i = 4; i <= n; i++) {
+//             arr[i] = arr[i-1] + arr[i-2];
+//         }
+        
+//         int result = arr[n];
+        
+//         delete[] arr;
+        
+//         return result;
         
     }
 };
