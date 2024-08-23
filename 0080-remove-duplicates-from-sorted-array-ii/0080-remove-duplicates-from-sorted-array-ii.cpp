2698ef724c70dc5193879d4211b6641c77f67773
Gaurav <gauravsanghu@yahoo.com>
2024-08-23 00:36:41 -0400

    Time: 4 ms (66.60%), Space: 13.8 MB (20.78%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -2,47 +2,70 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         
-        unordered_map<int, int> myMap;
+        // much more efficient solution
+        int n = nums.size();
         
-        int counter = nums.size();
+        if (n < 3) {
+            return n;
+        }
         
-        for(int n : nums) {
-            if (myMap.find(n) == myMap.end()) {
-                myMap[n] = 1;
-            }
-            else {
-                myMap[n]++;
+        int x = 2;
+        
+        
+        for(int y = 2; y < n; y++) {
+            if (nums[y] != nums[x-2]) {
+                nums[x] = nums[y];
+                x++;
             }
         }
         
-        for (int i = 0; i < nums.size(); i++) {
-            if (myMap[nums[i]] > 2) {
-                myMap[nums[i]]--;
-                counter--;
-                nums[i] = INT_MIN;
-            }
-        }
+        return x;
         
-        int i = 0;
         
-        while (i < nums.size()) {
-            int j = i + 1;
-            if (nums[i] == INT_MIN) {
-                while (j < nums.size() && nums[j] == INT_MIN) {
-                    j++;
-                }
-                if (j == nums.size()) {
-                    break;
-                }
-                swap(nums[i], nums[j]);    
-            }
-            if (j == nums.size()) {
-                    break;
-                }
-            i++;
-        }
         
-        return counter;
+        
+        // longer and more complex solution
+//         unordered_map<int, int> myMap;
+        
+//         int counter = nums.size();
+        
+//         for(int num : nums) {
+//             if (myMap.find(num) == myMap.end()) {
+//                 myMap[num] = 1;
+//             }
+//             else {
+//                 myMap[num]++;
+//             }
+//         }
+        
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (myMap[nums[i]] > 2) {
+//                 myMap[nums[i]]--;
+//                 counter--;
+//                 nums[i] = INT_MIN;
+//             }
+//         }
+        
+//         int i = 0;
+        
+//         while (i < nums.size()) {
+//             int j = i + 1;
+//             if (nums[i] == INT_MIN) {
+//                 while (j < nums.size() && nums[j] == INT_MIN) {
+//                     j++;
+//                 }
+//                 if (j == nums.size()) {
+//                     break;
+//                 }
+//                 swap(nums[i], nums[j]);    
+//             }
+//             if (j == nums.size()) {
+//                     break;
+//                 }
+//             i++;
+//         }
+        
+//         return counter;
         
     }
 };
