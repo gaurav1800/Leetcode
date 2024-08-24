d221be1d4231edc9ee4ce91cba350e535b173daa
Gaurav <gauravsanghu@yahoo.com>
2024-08-24 01:16:22 -0400

    Time: 0 ms (100.00%), Space: 10.6 MB (33.68%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -16,56 +16,33 @@ public:
             return head;
         }
         
-//         ListNode *current = new ListNode(0, head);
+        ListNode *dummy = new ListNode(0, head);
         
-//         ListNode *result = current;
-        
-//         int counter = 1;
-        
-//         while (counter+1 != left) {
-//             current = current->next;
-//             counter++;
-//         }
+        ListNode *current = dummy;
         
         
-//         ListNode *prev = current;
-//         current = current->next;
-//         ListNode *next = nullptr;
-        
-//         while (current != nullptr && counter != right) {
-//             next = current->next;
-//             current->next = prev;
-//             prev = current;
-//             current = next;
-//             counter++;
-//         }
-        
-//         result
-//         return result->next;
-        
-        ListNode dummy(0); // Create a dummy node
-        dummy.next = head;
-        ListNode* prev = &dummy;
-
-        // Move prev to the node just before the left-th node
-        for (int i = 1; i < left; ++i) {
-            prev = prev->next;
+        for(int i = 0; i < left-1; i++) {
+            current = current->next;
         }
-
-        // Reverse the sublist between left and right
-        ListNode* current = prev->next;
-        ListNode* next = nullptr;
-        ListNode* reverse_tail = current; // The tail of the reversed sublist
         
-        for (int i = left; i < right; ++i) {
-            next = current->next;
+        
+        ListNode *prev = current;
+        current = current->next;
+        
+        for(int i = left; i < right; i++) {
+            ListNode *next = current->next;
             current->next = next->next;
             next->next = prev->next;
             prev->next = next;
         }
-
-        // Return the new head
-        return dummy.next;
+        
+        return dummy->next;  
+        
+        
+        
+        
+        
+     
         
         
         
