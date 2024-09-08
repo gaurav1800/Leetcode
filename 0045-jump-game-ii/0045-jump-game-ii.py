805e7f08c4e51c1490c98ff74e995f36897dd8ac
Gaurav <gauravsanghu@yahoo.com>
2024-09-08 01:24:15 -0400

    Time: 106 ms (56.83%), Space: 17.5 MB (96.24%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        result = 0
+        
+        reach = 0
+        
+        end = 0
+        
+        for i in range(len(nums) - 1):
+            
+            reach = max(reach, i+nums[i])
+            
+            if reach >= len(nums)-1:
+                result += 1
+                break
+            
+            if i == end:
+                result += 1
+                end = reach
+        
+        return result
