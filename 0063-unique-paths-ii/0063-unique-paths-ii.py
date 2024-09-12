1d337cd767486fb6f3dd3c0b07117c97673e36eb
Gaurav <gauravsanghu@yahoo.com>
2024-09-12 03:23:35 -0400

    Time: 41 ms (76.18%), Space: 16.5 MB (97.95%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,17 +1,41 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         
+        # 1d array extra space solution
+        
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
         
-        dp = [[0] * (n+1) for i in range(m+1)]
-        dp[1][0] = 1
+        dp = [0] * (n)
+        dp[0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                
+                elif j > 0:
+                    dp[j] += dp[j-1]
+                    
+                
+        
+        return dp[n-1]
+        
+        
+#         # 2d array extra space solution
+        
+#         m = len(obstacleGrid)
+#         n = len(obstacleGrid[0])
+        
+#         dp = [[0] * (n+1) for i in range(m+1)]
+#         dp[1][0] = 1
         
 
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if obstacleGrid[i-1][j-1] == 0:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+#         for i in range(1, m+1):
+#             for j in range(1, n+1):
+#                 if obstacleGrid[i-1][j-1] == 0:
+#                     dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
-        return dp[m][n]
+#         return dp[m][n]
                 
