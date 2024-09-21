149c422e061b596a2d6b44902ab9f7b19777c901
Gaurav <gauravsanghu@yahoo.com>
2024-09-21 00:54:24 -0400

    Time: 121 ms (73.57%), Space: 19 MB (98.42%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,24 +1,41 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        
-        
+
         n = len(ratings)
         
-        leftToRight = [0] * n
-        rightToLeft = [0] * n
-        
-        counter = n
+        arr = [1] * n
         
         for i in range(1, n):
             if ratings[i] > ratings[i-1]:
-                leftToRight[i] = leftToRight[i-1] + 1
+                arr[i] = arr[i-1] + 1
         
         for i in range(n-2, -1, -1):
-            if ratings[i] > ratings[i+1]:
-                rightToLeft[i] = rightToLeft[i+1] + 1
+            if ratings[i] > ratings[i+1] and arr[i] <= arr[i+1]:
+                arr[i] = arr[i+1] + 1
         
-        for i in range(n):
-            counter += max(leftToRight[i], rightToLeft[i])
+        return sum(arr)
         
-        return counter
+        
+        
+#         Solution with more space
+
+#         n = len(ratings)
+        
+#         leftToRight = [0] * n
+#         rightToLeft = [0] * n
+        
+#         counter = n
+        
+#         for i in range(1, n):
+#             if ratings[i] > ratings[i-1]:
+#                 leftToRight[i] = leftToRight[i-1] + 1
+        
+#         for i in range(n-2, -1, -1):
+#             if ratings[i] > ratings[i+1]:
+#                 rightToLeft[i] = rightToLeft[i+1] + 1
+        
+#         for i in range(n):
+#             counter += max(leftToRight[i], rightToLeft[i])
+        
+#         return counter
         
