bce32f894cd84f4bbc2b61b1268688d7e1e90738
Gaurav <gauravsanghu@yahoo.com>
2024-09-22 17:47:55 -0400

    Time: 258 ms (91.44%), Space: 16.7 MB (39.64%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -16,7 +16,8 @@ class Solution:
                 left = i - (result-1)//2
                 right = i + result//2
         
-        return s[int(left):int(right+1)]
+        # return s[int(left):int(right+1)]
+        return s[left:right+1]
         
         
     def expand(self, word, left, right):
