b3b4e5e441b5065bcdac00b71787a502ed33b736
Gaurav <gauravsanghu@yahoo.com>
2024-09-27 01:06:15 -0400

    Time: 86 ms (71.79%), Space: 20.9 MB (95.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,28 @@
-from collections import Counter
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        dict1 = {}
         
-        myCounter = Counter(nums)
+        for num in nums:
+            if num in dict1:
+                dict1[num] += 1
+            else:
+                dict1[num] = 1
+            
+        min_heap = []
         
-        result = myCounter.most_common(k)
+        for num, freq in dict1.items():
+            heapq.heappush(min_heap, (freq, num))
+            
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
         
-        result = [num for num, _ in result]
+        result = []
+        
+        for freq, num in min_heap:
+            result.append(num)
         
         return result
+        
+        
         
