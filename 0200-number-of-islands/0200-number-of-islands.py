1783640603eba0db5299ace2842255141c717c2b
Gaurav <gauravsanghu@yahoo.com>
2024-09-28 00:51:26 -0400

    Time: 241 ms (63.27%), Space: 27.2 MB (14.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        if not grid or not grid[0]:
+        if not grid:
             return 0
         
         islands = 0
         
-        visit = set()
-        
         rows = len(grid)
-        columns = len(grid[0])
+        cols = len(grid[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
         
-        def dfs(r, c):
-            if (
-                r not in range(rows)
-                or c not in range(columns)
-                or grid[r][c] == "0"
-                or (r, c) in visit
-            ):
-                return
-            
-            visit.add((r, c))
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        def dfs(x, y):
+            if 0 <= x < rows and 0 <= y < cols and (x, y) not in visited and grid[x][y] == "1":
+                visited.add((x, y))
+                for dx, dy in directions:
+                    dfs(x+dx, y+dy)
         
-            for dr, dc in directions:
-                dfs(r+dr, c+dc)
-            
         for i in range(rows):
-            for j in range(columns):
-                if grid[i][j] == '1' and (i,j) not in visit:
+            for j in range(cols):
+                if grid[i][j] == "1" and (i, j) not in visited:
                     islands += 1
                     dfs(i, j)
         
         return islands
+            
+            
+        
+        
         
