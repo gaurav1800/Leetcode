507b0b1711f4ad1939c21f555a5b3ace9f4d2c39
Gaurav <gauravsanghu@yahoo.com>
2024-09-28 02:18:39 -0400

    Time: 729 ms (64.54%), Space: 27.5 MB (12.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,34 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-#         faster implementation
-        profit = 0
-        latestMin = prices[0]
+        
+        currentMin = inf
+        result = 0
         
         for price in prices:
-            if price - latestMin > profit:
-                profit = price - latestMin
-            if price < latestMin:
-                latestMin = price
+            if price < currentMin:
+                currentMin = price
+            else:
+                result = max(result, price-currentMin)
         
-        return profit
+        return result
         
-        
-        
-#         slow implementation
-
-#         left = 0
-#         right = 1
-        
-#         maxProfit = 0
-        
-#         while right < len(prices):
-#             if (prices[right] < prices[left]):
-#                 left = right
-#                 right += 1
-#                 continue
-#             else:
-#                 maxProfit = max(maxProfit, prices[right] - prices[left])
-#                 right += 1
-        
-#         return maxProfit
