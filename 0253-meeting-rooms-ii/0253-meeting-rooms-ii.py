e5e96b05e2c3752c1f8f203e8b4075c297854fc9
Gaurav <gauravsanghu@yahoo.com>
2024-09-29 19:43:28 -0400

    Time: 79 ms (28.26%), Space: 19.9 MB (90.12%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,24 +1,42 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        if len(intervals) == 1:
-            return 1
         
         intervals.sort(key = lambda x: x[0])
         
-        rooms = [intervals[0]]
-        total = 1
+        heap = []
+        
+        for i in range(len(intervals)):
+            start, end = intervals[i]
+            
+            if heap and heap[0] <= start:
+                heappop(heap)
+            
+            heappush(heap, end)
+        
+        return len(heap)
         
         
-        for i in range(1, len(intervals)):
-            j = 0
-            while j < len(rooms):
-                if rooms[j][1] <= intervals[i][0]:
-                    rooms[j] = intervals[i]
-                    break
-                else:
-                    j+=1
-            if j == len(rooms):
-                rooms.append(intervals[i])
+        
+#         # longer solution
+#         if len(intervals) == 1:
+#             return 1
+        
+#         intervals.sort(key = lambda x: x[0])
+        
+#         rooms = [intervals[0]]
+#         total = 1
+        
+        
+#         for i in range(1, len(intervals)):
+#             j = 0
+#             while j < len(rooms):
+#                 if rooms[j][1] <= intervals[i][0]:
+#                     rooms[j] = intervals[i]
+#                     break
+#                 else:
+#                     j+=1
+#             if j == len(rooms):
+#                 rooms.append(intervals[i])
                 
-        return len(rooms)
+#         return len(rooms)
