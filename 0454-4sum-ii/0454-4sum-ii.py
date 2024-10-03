131953429140738582c2225588687c59a5244e81
Gaurav <gauravsanghu@yahoo.com>
2024-10-03 15:03:09 -0400

    Time: 524 ms (19.59%), Space: 17 MB (11.72%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        
+        dict12 = {} 
+        dict34 = {}
+        
+        counter = 0
+        
+        for num1 in nums1:
+            for num2 in nums2:
+                if num1+num2 in dict12:
+                    dict12[num1+num2] += 1
+                else:
+                    dict12[num1+num2] = 1
+        
+        for num3 in nums3:
+            for num4 in nums4:
+                if num3+num4 in dict34:
+                    dict34[num3+num4] += 1
+                else:
+                    dict34[num3+num4] = 1
+        
+        for key, value in dict12.items():
+            if -key in dict34:
+                counter += dict12[key]*dict34[-key]
+        
+        return counter
+            
