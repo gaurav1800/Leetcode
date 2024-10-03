f2603e9dfeafcc7184e704c9f639ca3ba610984f
Gaurav <gauravsanghu@yahoo.com>
2024-10-03 00:11:28 -0400

    Time: 574 ms (62.19%), Space: 27.6 MB (70.47%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -3,8 +3,13 @@ class Solution:
         
         result = target[0]
         
-        for i in range(1, len(target)):
-            if target[i] > target[i-1]:
-                result += target[i]-target[i-1]
+        for x, y in zip(target, target[1:]):
+            if x < y:
+                result += y-x
+                
+        # Another solution
+        # for i in range(1, len(target)):
+        #     if target[i] > target[i-1]:
+        #         result += target[i]-target[i-1]
         
         return result
