d7cf5ccb5101d1443229c641e4335234f53e4c4e
Gaurav <gauravsanghu@yahoo.com>
2024-10-03 15:07:42 -0400

    Time: 350 ms (98.90%), Space: 17.7 MB (5.06%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,28 +1,46 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
         
-        dict12 = {} 
-        dict34 = {}
         
-        counter = 0
+        result = 0
         
-        for num1 in nums1:
-            for num2 in nums2:
-                if num1+num2 in dict12:
-                    dict12[num1+num2] += 1
-                else:
-                    dict12[num1+num2] = 1
+        d12 = Counter([num1 + num2 for num1 in nums1 for num2 in nums2])
+        d34 = Counter([num3 + num4 for num3 in nums3 for num4 in nums4])
         
-        for num3 in nums3:
-            for num4 in nums4:
-                if num3+num4 in dict34:
-                    dict34[num3+num4] += 1
-                else:
-                    dict34[num3+num4] = 1
+        for key, value in d12.items():
+            if -key in d34:
+                result += d34[-key] * value
         
-        for key, value in dict12.items():
-            if -key in dict34:
-                counter += dict12[key]*dict34[-key]
+        return result
         
-        return counter
+        
+        
+        
+        
+        
+        # longer solution
+#         dict12 = {} 
+#         dict34 = {}
+        
+#         counter = 0
+        
+#         for num1 in nums1:
+#             for num2 in nums2:
+#                 if num1+num2 in dict12:
+#                     dict12[num1+num2] += 1
+#                 else:
+#                     dict12[num1+num2] = 1
+        
+#         for num3 in nums3:
+#             for num4 in nums4:
+#                 if num3+num4 in dict34:
+#                     dict34[num3+num4] += 1
+#                 else:
+#                     dict34[num3+num4] = 1
+        
+#         for key, value in dict12.items():
+#             if -key in dict34:
+#                 counter += dict12[key]*dict34[-key]
+        
+#         return counter
             
