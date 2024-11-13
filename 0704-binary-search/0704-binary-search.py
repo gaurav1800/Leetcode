538d634158f1210af5a319ec8ddbfd2a98c55160
Gaurav <gauravsanghu@yahoo.com>
2024-11-12 20:07:44 -0500

    Time: 0 ms (100.00%), Space: 17.6 MB (97.59%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -4,13 +4,13 @@ class Solution:
         lo = 0
         hi = len(nums) - 1
         
-        while (lo <= hi):
+        while lo <= hi:
             mid = (lo+hi) // 2
             
             
-            if (target < nums[mid]):
+            if target < nums[mid]:
                 hi = mid-1
-            elif (target > nums[mid]):
+            elif target > nums[mid]:
                 lo = mid+1
             else:
                 return mid
