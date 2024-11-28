603685174164a5afa8d18301996658c958750d25
Gaurav <gauravsanghu@yahoo.com>
2024-11-27 19:54:44 -0500

    Time: 8 ms (78.66%), Space: 19.8 MB (21.66%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,29 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-from queue import PriorityQueue
-
-
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        dummy  = ListNode(0)
+        dummy = ListNode(0)
         current = dummy
-        
-        queue = PriorityQueue()
-        
-        for i, j in enumerate(lists):
-            if j:
-                queue.put((j.val, i, j))
-        
-        while not queue.empty():
-            _, i, minNode = queue.get()
-            if minNode.next:
-                queue.put((minNode.next.val, i, minNode.next))
-            current.next = minNode
-            current = current.next
-        
-        
+
+        heap = []
+
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node)) 
+
+        while heap:
+            val, i, minNode = heapq.heappop(heap)  
+            current.next = minNode  
+            current = current.next  
+            
+            if minNode.next: 
+                heapq.heappush(heap, (minNode.next.val, i, minNode.next))
+
         return dummy.next
-        
+
