b539926e568e11dc82f5950c7ad197e34d971b3a
Gaurav <gauravsanghu@yahoo.com>
2024-12-19 19:09:03 -0500

    Time: 3007 ms (9.13%), Space: 18.2 MB (7.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,63 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
+        size = len(s1)
+        s1Sorted = sorted(s1)
         
-#         faster implementation
-        if len(s1) > len(s2):
-            return False
-        
-        s1Count, s2Count = [0]*26, [0]*26
-        
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1 
-            s2Count[ord(s2[i]) - ord('a')] += 1 
-        
-        matches = 0
-        
-        for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        for i in range(len(s2)):
+            if i+size > len(s2):
+                return False
             
-        left = 0
-        for right in range(len(s1), len(s2)):
-            if matches == 26:
+            window = s2[i:i+size]
+            windowSorted = sorted(window)
+            
+            if s1Sorted == windowSorted:
                 return True
-            
-            idx = ord(s2[right]) - ord('a')
-            
-            s2Count[idx] += 1
-            if s2Count[idx] == s1Count[idx]:
-                matches += 1
-            elif s2Count[idx] - 1 == s1Count[idx]:
-                matches -= 1
-                
-                
-            idx = ord(s2[left]) - ord('a')
-            
-            s2Count[idx] -= 1
-            if s2Count[idx] == s1Count[idx]:
-                matches += 1
-            elif s2Count[idx] + 1 == s1Count[idx]:
-                matches -= 1
-                
-            left += 1
-            
-        return matches == 26
-                                
-            
         
-        
-#         slow implementation
-#         size = len(s1)
-#         s1Sorted = sorted(s1)
-        
-#         for i in range(len(s2)):
-#             if i+size > len(s2):
-#                 return False
-            
-#             window = s2[i:i+size]
-#             windowSorted = sorted(window)
-            
-#             if s1Sorted == windowSorted:
-#                 return True
-        
-#         return False
+        return False
