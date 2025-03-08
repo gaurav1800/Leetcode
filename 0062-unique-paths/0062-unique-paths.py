43f9ac196c7f19f0cd1270b6cb5dfd21c52fe0ee
Gaurav <gauravsanghu@yahoo.com>
2025-03-08 10:19:37 -0500

    Time: 0 ms (100%), Space: 17.6 MB (88.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-            
+        
         # 1d array solution
         dp = [1] * n
-        
         for i in range(1, m):
             for j in range(1, n):
                 dp[j] += dp[j-1]
         
         return dp[-1]
-        
+
         
         # 2d array solution
 #         dp = [[1] * n for i in range(m)]
@@ -19,5 +18,3 @@ class Solution:
 #                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
 #         return dp[-1][-1]
-    
-        
