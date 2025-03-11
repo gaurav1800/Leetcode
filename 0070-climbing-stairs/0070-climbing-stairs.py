20a240741a377e66b87a2bffc60991452f4479ff
Gaurav <gauravsanghu@yahoo.com>
2025-03-11 18:39:09 -0400

    Time: 0 ms (100%), Space: 17.6 MB (95.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -11,16 +11,13 @@ class Solution:
             temp = first+second
             first = second
             second = temp
-        
+
         return second
         
         
         
 #         # another solution
 #         array = [1, 2, 3]
-        
 #         for i in range(3, n):
 #             array.append(array[i-1] + array[i-2])
-        
 #         return array[len(array)-1]
-        
