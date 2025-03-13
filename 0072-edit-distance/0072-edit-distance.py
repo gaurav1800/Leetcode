8bbfed1331adf1e280bc78b93cda24887816e649
Gaurav <gauravsanghu@yahoo.com>
2025-03-12 20:52:13 -0400

    Time: 69 ms (43.11%), Space: 21.2 MB (58.3%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -4,12 +4,10 @@ class Solution:
         len1 = len(word1)
         len2 = len(word2)
         
-        
         dp = [[0] * (len2+1) for _ in range(len1+1)]
         
         for i in range(1, len1+1):
             dp[i][0] = i
-        
         
         for j in range(1, len2+1):
             dp[0][j] = j
