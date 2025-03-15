39bccb532827c8760507cb2ab235ad7d157cae80
Gaurav <gauravsanghu@yahoo.com>
2025-03-15 11:40:45 -0400

    Time: 0 ms (100%), Space: 17.9 MB (16.95%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(numRows):
             result.append([1] * (i+1))
         
-        for i in range(2, len(result)):
+        for i in range(2, numRows):
             for j in range(1, len(result[i])-1):
                 result[i][j] = result[i-1][j-1] + result[i-1][j]
         
