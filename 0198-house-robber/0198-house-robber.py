0c845ac3f5d40e0cde63e2ef623d3061d5ebcc68
Gaurav <gauravsanghu@yahoo.com>
2025-03-19 18:57:58 -0400

    Time: 0 ms (100%), Space: 18 MB (13.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,11 +3,9 @@ class Solution:
         
         if len(nums) < 3:
             return max(nums)
-        
+
         nums[2] += nums[0]
-        
         for i in range(3, len(nums)):
             nums[i] += max(nums[i-2], nums[i-3])
 
         return max(nums[-1], nums[-2])
-        
