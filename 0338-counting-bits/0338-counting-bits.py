7e37b90cb90bc82e737708e3ae61ae351d8bd11f
Gaurav <gauravsanghu@yahoo.com>
2025-03-24 18:51:10 -0400

    Time: 7 ms (52.91%), Space: 18.6 MB (22.61%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,13 +2,11 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         
         array = [0] * (n+1)
-        
         offset = 1
         
         for num in range(1, n+1):
             if offset*2 == num:
                 offset = num
-            
             array[num] = 1 + array[num - offset]
         
         return array
