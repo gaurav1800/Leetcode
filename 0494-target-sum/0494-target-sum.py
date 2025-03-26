00fbca0a0dbe0fe10dbcfb13e72f158552fca01d
Gaurav <gauravsanghu@yahoo.com>
2025-03-26 18:19:13 -0400

    Time: 43 ms (87.47%), Space: 17.7 MB (99.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,16 +2,13 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
         total = sum(nums)
-        
         if total < abs(target) or (total + target) % 2 == 1:
             return 0
         
         return self.helper(nums, (total+target) // 2, total)
     
     def helper(self, nums, target, total):
-        
         dp = [1] + [0] * total
-        
         for i in range(len(nums)):
             for j in range(total, nums[i] - 1, -1):
                 dp[j] += dp[j - nums[i]]
