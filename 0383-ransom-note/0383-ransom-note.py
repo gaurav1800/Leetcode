058ef2517e1ac16a01f05706ffafe0d7f2778392
Gaurav <gauravsanghu@yahoo.com>
2025-06-08 09:53:04 -0400

    Time: 5 ms (94.15%), Space: 17.8 MB (81.95%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -4,18 +4,28 @@ class Solution:
         if len(ransomNote) > len(magazine):
             return False
         
-        hashMap = {}
+        mySet = set(ransomNote)
 
-        for c in magazine:
-            if c in hashMap:
-                hashMap[c] += 1
-            else:
-                hashMap[c] = 1
-        
-        for c in ransomNote:
-            if c in hashMap and hashMap[c] > 0:
-                hashMap[c] -= 1
-            else:
+        for c in mySet:
+            if ransomNote.count(c) > magazine.count(c):
                 return False
-        
+
         return True
+
+
+        # # slow solution
+        # hashMap = {}
+
+        # for c in magazine:
+        #     if c in hashMap:
+        #         hashMap[c] += 1
+        #     else:
+        #         hashMap[c] = 1
+        
+        # for c in ransomNote:
+        #     if c in hashMap and hashMap[c] > 0:
+        #         hashMap[c] -= 1
+        #     else:
+        #         return False
+        
+        # return True
