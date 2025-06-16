933b3573a487de7dfa346533ddc0c552a2a63603
Gaurav <gauravsanghu@yahoo.com>
2025-06-16 18:27:38 -0400

    Time: 28 ms (70.3%), Space: 36.6 MB (58.48%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,24 +1,36 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         
-        if len(nums) < k:
-            return len(set(nums)) != len(nums)
-        queue = deque()
-        seen = set()
-
-        for n in nums:
-            if len(queue) != k+1:
-                queue.append(n)
-                seen.add(n)
-            else:
-                temp = queue.popleft()
-                seen.remove(temp)
-                queue.append(n)
-                seen.add(n)
-
-            if len(queue) != len(seen):
+        # fast solution
+        seen = {}
+        for i, num in enumerate(nums):
+            if num in seen and abs(seen[num]-i) <= k:
                 return True
+            else:
+                seen[num] = i
+
         return False
+        
+        # slow solution
+
+        # if len(nums) < k:
+        #     return len(set(nums)) != len(nums)
+        # queue = deque()
+        # seen = set()
+
+        # for n in nums:
+        #     if len(queue) != k+1:
+        #         queue.append(n)
+        #         seen.add(n)
+        #     else:
+        #         temp = queue.popleft()
+        #         seen.remove(temp)
+        #         queue.append(n)
+        #         seen.add(n)
+
+        #     if len(queue) != len(seen):
+        #         return True
+        # return False
 
 
 
