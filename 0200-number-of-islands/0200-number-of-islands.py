7158befe1eec1f01995f630757bda2a859550847
Gaurav <gauravsanghu@yahoo.com>
2025-07-20 11:07:08 -0400

    Time: 254 ms (47%), Space: 20.2 MB (57.5%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -16,17 +16,9 @@ class Solution:
                         grid[nx+dx][ny+dy] = "0"
         
         result = 0
-        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     bfs(i, j)
                     result += 1
-        
         return result
-        
-            
-            
-        
-        
-        
