c2a7125e71777535ece5a7783c2269a4a4b50fa2
Gaurav <gauravsanghu@yahoo.com>
2025-08-10 10:14:16 -0400

    Time: 16 ms (27.95%), Space: 17.9 MB (25.57%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -4,7 +4,8 @@ class Solution:
         if n == 1:
             return True
 
-        if n <= 0 or n%3 != 0:
+        elif n <= 0 or n%3 != 0:
             return False
-            
-        return self.isPowerOfThree(n/3)
+
+        else:
+            return self.isPowerOfThree(n/3)
