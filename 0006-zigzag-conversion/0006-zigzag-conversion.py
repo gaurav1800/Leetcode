362b7308b887ee7b28821e192ca2701145edcd67
Gaurav <gauravsanghu@yahoo.com>
2025-08-23 11:18:43 -0400

    Time: 11 ms (62.28%), Space: 17.9 MB (69.47%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,11 +2,8 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         
         result = ["" for _ in range(numRows)]
-        
         row = 0
-        
         direction = 0 if numRows == 1 else 1
-        
         
         for c in s:
             result[row] += c
