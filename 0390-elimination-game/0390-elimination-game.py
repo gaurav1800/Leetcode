f373b49734e3bed0ee4ed0fee08e74215e4694cd
Gaurav <gauravsanghu@yahoo.com>
2025-10-03 16:44:44 -0400

    Time: 0 ms (100%), Space: 18 MB (28.76%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -1,16 +1,24 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
         
-        head = 1
-        step = 1
-        left = True
-        remaining = n
-
-        while remaining > 1:
-            if left or remaining%2 == 1:
-                head += step
-            step *= 2
-            remaining //= 2
-            left = not left
+        # Recursive solutoin
+        if n == 1:
+            return 1
         
-        return head
+        return 2 * (n//2  + 1 - self.lastRemaining(n//2))
+
+
+        # longer solution
+        # head = 1
+        # step = 1
+        # left = True
+        # remaining = n
+
+        # while remaining > 1:
+        #     if left or remaining%2 == 1:
+        #         head += step
+        #     step *= 2
+        #     remaining //= 2
+        #     left = not left
+        
+        # return head
