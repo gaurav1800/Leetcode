8169a149152f89af243547db2c527e35e4e6975f
Gaurav <gauravsanghu@yahoo.com>
2025-10-05 23:23:56 -0400

    Time: 11 ms (90.47%), Space: 19.9 MB (69.02%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -13,24 +13,3 @@ class Solution:
                 arr[i] = arr[i+1] + 1
         
         return sum(arr)
-        
-        
-        
-#         Solution with more space
-#         n = len(ratings)
-#         leftToRight = [0] * n
-#         rightToLeft = [0] * n
-#         counter = n
-        
-#         for i in range(1, n):
-#             if ratings[i] > ratings[i-1]:
-#                 leftToRight[i] = leftToRight[i-1] + 1
-        
-#         for i in range(n-2, -1, -1):
-#             if ratings[i] > ratings[i+1]:
-#                 rightToLeft[i] = rightToLeft[i+1] + 1
-        
-#         for i in range(n):
-#             counter += max(leftToRight[i], rightToLeft[i])
-        
-#         return counter
