3579be228c77c170818f49eff9ec40004bbd4b22
Gaurav <gauravsanghu@yahoo.com>
2025-10-18 13:04:39 -0400

    Time: 4 ms (15.39%), Space: 17.7 MB (78.09%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,6 +1,15 @@
 class Solution:
-    def myAtoi(self, s: str) -> int:
+    def reset(self, s, i):
+            s = s[i::]
+            i = 0
+            return s, i
 
+    def checkIfOverLength(self, s, i):
+        if i == len(s): 
+            return True
+        return False
+
+    def myAtoi(self, s: str) -> int:
         i = 0
         negative = False
 
@@ -8,35 +17,40 @@ class Solution:
         while i < len(s) and s[i] == " ":
             i += 1
         
-        if i >= len(s): return 0
-        
-        s = s[i::]
-        i = 0
+        if self.checkIfOverLength(s, i): return 0
+        s, i = self.reset(s, i)
       
         # Signedness
         if s[i] == "-" or s[i] == "+":
             if s[i] == "-":
                 negative = True
             i += 1
-            s = s[i::]
-            i = 0
+            s, i = self.reset(s, i)
         
         while i < len(s) and s[i] == "0":
             i += 1
         
-        if i >= len(s) or not s[i].isdigit(): return 0
+        if self.checkIfOverLength(s, i) or not s[i].isdigit(): return 0
 
-        s = s[i::]
-        i = 0
+        s, i = self.reset(s, i)
 
+        # Conversion
         while i < len(s) and s[i].isdigit():
             i += 1
 
         result = int(s[:i:]) if negative is False else int(s[:i:])*-1
 
+        # Rounding
         if result < -2**31: return -2**31
         elif result > 2**31 - 1: return 2**31 - 1
         else: return result
+
+
+
+        
+
+        
+
 
 
 
