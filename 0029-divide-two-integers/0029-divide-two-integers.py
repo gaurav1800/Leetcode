2aaac7a410603bbeacb770bb79a7f09fc6ac17f8
Gaurav <gauravsanghu@yahoo.com>
2025-10-26 15:22:53 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -4,22 +4,22 @@ class Solution:
         INT_MAX = 2**31 - 1
         INT_MIN = -2**31
 
-        if dividend == INT_MIN and divisor ==  -1:
+        if dividend <= INT_MIN and divisor == -1:
             return INT_MAX
-
+        
         negativeFlag = (dividend < 0) ^ (divisor < 0)
+        quotient = 0
         dividend = abs(dividend)
         divisor = abs(divisor)
-        quotient = 0
 
-        while dividend >= divisor:
-            tmp, multiple = divisor, 1
+        while divisor <= dividend:
+            tmp = divisor
+            multiple = 1
             while dividend >= (tmp << 1):
                 tmp <<= 1
                 multiple <<= 1
-            dividend -= tmp
+            divisor -= tmp
             quotient += multiple
-        
-        return -quotient if negativeFlag else quotient 
 
+        return -quotient if negativeFlag else quotient
         
