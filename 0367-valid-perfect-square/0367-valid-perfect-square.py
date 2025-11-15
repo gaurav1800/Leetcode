03c6381c62514aaca44b5ca912e0e34b6578723b
Gaurav <gauravsanghu@yahoo.com>
2025-11-15 18:56:26 -0500

    Time: 0 ms (100%), Space: 17.8 MB (31.25%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -4,7 +4,7 @@ class Solution:
         if num == 1:
             return True
         
-        lo, hi = 2, num//2
+        lo, hi = 2, num
 
         while lo <= hi:
             mid = (lo + hi) // 2
