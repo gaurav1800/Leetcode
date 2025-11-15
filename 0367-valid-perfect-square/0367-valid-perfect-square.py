bafea237fe3bfcac2c120d67edb6694a2aff4180
Gaurav <gauravsanghu@yahoo.com>
2025-11-15 18:53:00 -0500

    Time: 0 ms (100%), Space: 17.7 MB (80.11%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -1,5 +1,19 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         
-        n = bisect.bisect_left(range(num), num, key = lambda f: f*f)
-        return n**2 == num
+        if num == 1:
+            return True
+        
+        lo, hi = 2, num//2
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            midSquared = mid ** 2
+            if midSquared == num:
+                return True
+            elif midSquared < num:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        
+        return False
