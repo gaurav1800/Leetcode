d81897108457017f792045e9e5ad9f3d7ba4ac31
Gaurav <gauravsanghu@yahoo.com>
2025-11-24 11:24:29 -0500

    Time: 9 ms (93.56%), Space: 19.9 MB (68.99%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -2,14 +2,14 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         
         n = len(ratings)
-        arr = [1] * n
+        candies = [1] * n
         
         for i in range(1, n):
             if ratings[i] > ratings[i-1]:
-                arr[i] = arr[i-1] + 1
+                candies[i] = candies[i-1] + 1
         
         for i in range(n-2, -1, -1):
-            if ratings[i] > ratings[i+1] and arr[i] <= arr[i+1]:
-                arr[i] = arr[i+1] + 1
+            if ratings[i] > ratings[i+1] and candies[i] <= candies[i+1]:
+                candies[i] = candies[i+1] + 1
         
-        return sum(arr)
+        return sum(candies)
