1570e6469017252473ff4f39b1b3278939c1333f
Gaurav <gauravsanghu@yahoo.com>
2026-01-25 14:10:19 +0800

    Time: 7 ms (68.69%), Space: 19.6 MB (8.12%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,25 +1,8 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         
-        set1 = set()
-        set2 = set()
-        set3 = set()
-        set4 = set()
+        set1 = set(nums1)
+        set2 = set(nums2)
 
-        for i in nums1:
-            set1.add(i)
-
-        for i in nums2:
-            set2.add(i)
-            
-        for n in set1:
-            if n not in set2:
-                set3.add(n)
+        return [list(set1-set2), list(set2-set1)]
         
-        for n in set2:
-            if n not in set1:
-                set4.add(n)
-        
-        return [list(set3), list(set4)]
-
-
