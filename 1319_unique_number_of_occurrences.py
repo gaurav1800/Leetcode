1b0a10501ac7f465d37f042625d1cdbccfe0ac39
Gaurav <gauravsanghu@yahoo.com>
2026-02-01 02:30:20 +0800

    Add 1319_unique_number_of_occurrences.py - LeetCode Problem 1319

---
+++ 1319_unique_number_of_occurrences.py
@@ -0,0 +1,22 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        
+        # simpler and shorter solution
+        count = Counter(arr)
+        return len(count) == len(set(count.values()))
+
+
+        # kv = {}
+        # occSet = set()
+
+        # for num in arr:
+        #     if num not in kv:
+        #         kv[num] = 1
+        #     else:
+        #         kv[num] += 1
+        
+        # for val in kv.values():
+        #     if val in occSet:
+        #         return False
+        #     occSet.add(val)
+        # return True
