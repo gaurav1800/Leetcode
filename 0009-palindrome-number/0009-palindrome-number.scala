7250361130269ef54f2566ec1cf3adab236b5493
Gaurav <gauravsanghu@yahoo.com>
2026-02-08 15:24:59 -0500

    Time: 5 ms (94.95%), Space: 55.9 MB (55.56%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.scala
@@ -0,0 +1,23 @@
+import scala.annotation.tailrec
+
+object Solution {
+    def isPalindrome(x: Int): Boolean = {
+        
+        if (x < 0 || x % 10 == 0 && x != 0) {
+            false
+        } else {
+            @tailrec
+            def reverse(currentX: Int, currentReversed: Int): Int = {
+                if (currentX == 0) {
+                    currentReversed
+                } else {
+                    val lastDigit = currentX % 10
+                    val nextReversed = (currentReversed * 10) + lastDigit
+                    reverse(currentX / 10, nextReversed)
+                }
+            }
+            reverse(x, 0) == x
+        }
+
+    }
+}
