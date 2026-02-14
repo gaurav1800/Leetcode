ac36328455170b6f4835221f6f373f6d4bf4a497
Gaurav <gauravsanghu@yahoo.com>
2026-02-13 22:48:13 -0500

    Time: 42 ms (13.33%), Space: 58.8 MB (74.17%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.scala
@@ -0,0 +1,12 @@
+object Solution {
+    def longestCommonPrefix(strs: Array[String]): String = {
+        
+        if (strs.isEmpty) return ""
+
+        strs.reduce((s1, s2) => {
+            s1.zip(s2).takeWhile { case (c1, c2) => c1 == c2}
+                .map(_._1)
+                .mkString
+        })
+    }
+}
