8401564d64a9a9f33452af4b800f6d42693894f1
Gaurav <gauravsanghu@yahoo.com>
2026-02-14 22:27:07 -0500

    Time: 24 ms (48.98%), Space: 58.3 MB (32.65%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.scala
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.scala
@@ -1,9 +1,16 @@
 object Solution {
     def strStr(haystack: String, needle: String): Int = {
 
-        
+
         // simpler
-        haystack.indexOf(needle)
-        
+        // haystack.indexOf(needle)
+
+        // functional way
+        val n = haystack.length
+        val m = needle.length
+
+        (0 to n - m).find { i =>
+            haystack.substring(i, i + m) == needle
+        }.getOrElse(-1)
     }
 }
