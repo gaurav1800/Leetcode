9046304d66f0ce08d0e5b9c8bd894fa131deec43
Gaurav <gauravsanghu@yahoo.com>
2026-02-14 21:57:14 -0500

    Time: 7 ms (39.74%), Space: 60.4 MB (5.13%) - LeetHub

--- 0027-remove-element/0027-remove-element.scala
+++ 0027-remove-element/0027-remove-element.scala
@@ -1,26 +1,26 @@
 object Solution {
     def removeElement(nums: Array[Int], `val`: Int): Int = {
         
-        nums.indices.foldLeft(0) {(i, j) =>
+        if (nums.isEmpty) return 0
+
+        var i = 0
+        for (j <- 0 until nums.length) {
             if (nums(j) != `val`) {
                 nums(i) = nums(j)
-                i + 1
-            }
-            else {
-                i
+                i += 1
             }
         }
-
-        // slower
-        // if (nums.isEmpty) return 0
-
-        // var i = 0
-        // for (j <- 0 until nums.length) {
+        i
+        
+        // slower with foldLeft
+        // nums.indices.foldLeft(0) {(i, j) =>
         //     if (nums(j) != `val`) {
         //         nums(i) = nums(j)
-        //         i += 1
+        //         i + 1
+        //     }
+        //     else {
+        //         i
         //     }
         // }
-        // i
     }
 }
