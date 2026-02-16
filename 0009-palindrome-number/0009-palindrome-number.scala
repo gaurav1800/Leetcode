0a99356a6d3b5cf8646611c50cceadcbae4a5889
Gaurav <gauravsanghu@yahoo.com>
2026-02-15 22:50:25 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.scala
+++ 0009-palindrome-number/0009-palindrome-number.scala
@@ -1,8 +1,5 @@
-import scala.annotation.tailrec
-
 object Solution {
     def isPalindrome(x: Int): Boolean = {
-        
         if (x < 0 || x % 10 == 0 && x != 0) {
             false
         } else {
@@ -18,6 +15,6 @@ object Solution {
             }
             reverse(x, 0) == x
         }
-
+        
     }
 }
