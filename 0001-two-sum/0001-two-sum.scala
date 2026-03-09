cf2356bdceeb07285d27cfb762839745f98b62bc
Gaurav <gauravsanghu@yahoo.com>
2026-03-08 21:34:14 -0400

    Time: 23 ms (73.53%), Space: 61.5 MB (30%) - LeetHub

--- 0001-two-sum/0001-two-sum.scala
+++ 0001-two-sum/0001-two-sum.scala
@@ -2,7 +2,7 @@ import scala.annotation.tailrec
 
 object Solution {
     def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-        
+
         @tailrec
         def solve(index: Int, seen: Map[Int, Int]): Array[Int] = {
             val complement = target - nums(index)
