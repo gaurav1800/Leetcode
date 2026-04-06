3e78a06e9bca560d9edf2891fa8e033df283c736
Gaurav <gauravsanghu@yahoo.com>
2026-04-05 20:19:22 -0400

    Time: 0 ms (100%), Space: 6 MB (36.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,10 +1,10 @@
 func twoSum(nums []int, target int) []int {
+    
     numsMap := make(map[int]int)
 
     for i, num := range nums {
         complement := target - num
 
-        // if complement in map
         if index, found := numsMap[complement]; found {
             return []int{index, i}
         }
