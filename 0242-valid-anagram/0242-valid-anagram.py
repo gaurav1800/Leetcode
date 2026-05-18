b594282fd695f5a077f48b56d3c91f9ee811a4e3
Gaurav <gauravsanghu@yahoo.com>
2026-05-17 20:43:56 -0400

    Time: 3 ms (98.27%), Space: 19.3 MB (94.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,34 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        return Counter(s) == Counter(t)
+        # shorter soln
+        if len(s) != len(t):
+            return False
+        
+        chars = set(s)
+
+        for c in chars:
+            if s.count(c) != t.count(c):
+                return False
+        return True
+
+
+        # longer soln        
+        # kvMap = {}
+        
+        # for c in s:
+        #     if c in kvMap:
+        #         kvMap[c] += 1
+        #     else:
+        #         kvMap[c] = 1
+        # for c in t:
+        #     if c not in kvMap:
+        #         return False
+        #     kvMap[c] -= 1
+        # for k, v in kvMap.items():
+        #     if kvMap[k] != 0:
+        #         return False
+        
+        # return True
+
+        
