0ac3b584589b677ffa7a181e72973a0273fc2444
Gaurav <gauravsanghu@yahoo.com>
2026-05-17 20:36:51 -0400

    Time: 21 ms (54.35%), Space: 10.8 MB (75.61%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,12 @@
+func containsDuplicate(nums []int) bool {
+    set := make(map[int]struct{})
+
+    for _, num := range nums {
+        if _, exists := set[num]; exists {
+            return true
+        }
+        set[num] = struct{}{}
+    }
+
+    return false
+}
